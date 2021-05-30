4ed323e877ecedf3fbe432fa698c966b2eeb2927
dangellotto <dany.ange@hotmail.com>
2021-05-30 12:18:21 +0200

    Aggiunta classe TrafficLightDetection
    Aggiunta classe "TrafficLightDetection" per la gestione della detection del semaforo e per il calcolo delle fence del semaforo

---
+++ traffic_light_detection.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import cos, sin, pi,tan
+from cutils import CUtils
+
+#   Required to import carla library
+import os
+import sys
+sys.path.append(os.path.abspath(sys.path[0] + '/..'))
+
+# Utils : X - Rotation
+def rotate_x(angle):
+    R = np.mat([[ 1,         0,           0],
+                 [ 0, cos(angle), -sin(angle) ],
+                 [ 0, sin(angle),  cos(angle) ]])
+    return R
+
+# Utils : Y - Rotation
+def rotate_y(angle):
+    R = np.mat([[ cos(angle), 0,  sin(angle) ],
+                 [ 0,         1,          0 ],
+                 [-sin(angle), 0,  cos(angle) ]])
+    return R
+
+# Utils : Z - Rotation
+def rotate_z(angle):
+    R = np.mat([[ cos(angle), -sin(angle), 0 ],
+                 [ sin(angle),  cos(angle), 0 ],
+                 [         0,          0, 1 ]])
+    return R
+
+# Utils : Rotation - XYZ
+def to_rot(r):
+    Rx = np.mat([[ 1,         0,           0],
+                 [ 0, cos(r[0]), -sin(r[0]) ],
+                 [ 0, sin(r[0]),  cos(r[0]) ]])
+
+    Ry = np.mat([[ cos(r[1]), 0,  sin(r[1]) ],
+                 [ 0,         1,          0 ],
+                 [-sin(r[1]), 0,  cos(r[1]) ]])
+
+    Rz = np.mat([[ cos(r[2]), -sin(r[2]), 0 ],
+                 [ sin(r[2]),  cos(r[2]), 0 ],
+                 [         0,          0, 1 ]])
+
+    return Rz*Ry*Rx
+
+class TrafficLightDetection:
+    def __init__(self, camera_parameters):
+        # Constants
+        self.NUM_SEMAPHORE_CHECKS = 10
+        self.SCORE_THRESHOLD = 0.35
+        
+        # Detection parameters
+        self.prev_semaphore_box = None
+        self.count_semaphore_detections = 0
+        self.count_missdetection = 0
+        self.th = int(0.10 * camera_parameters["width"])
+
+        # Camera parameters
+        self.cam_height = camera_parameters['z']
+        self.cam_x_pos = camera_parameters['x']
+        self.cam_y_pos = camera_parameters['y']
+
+        self.cam_yaw = camera_parameters['yaw'] 
+        self.cam_pitch = camera_parameters['pitch'] 
+        self.cam_roll = camera_parameters['roll']
+        
+        self.camera_width = camera_parameters['width']
+        self.camera_height = camera_parameters['height']
+
+        self.camera_fov = camera_parameters['fov']
+
+        # Calculate Intrinsic Matrix
+        f = self.camera_width /(2 * tan(self.camera_fov * pi / 360))
+        Center_X = self.camera_width / 2.0
+        Center_Y = self.camera_height / 2.0
+
+        intrinsic_matrix = np.array([[f, 0, Center_X],
+                                     [0, f, Center_Y],
+                                     [0, 0, 1]])
+                                      
+        self.inv_intrinsic_matrix = np.linalg.inv(intrinsic_matrix)
+
+        # Rotation matrix to align image frame to camera frame
+        rotation_image_camera_frame = np.dot(rotate_z(-90 * pi /180),rotate_x(-90 * pi /180))
+
+        image_camera_frame = np.zeros((4,4))
+        image_camera_frame[:3,:3] = rotation_image_camera_frame
+        image_camera_frame[:, -1] = [0, 0, 0, 1]
+
+        # Lambda Function for transformation of image frame in camera frame 
+        self.image_to_camera_frame = lambda object_camera_frame: np.dot(image_camera_frame, object_camera_frame)
+
+    
+    def detect(self, traffic_light_detector, image, ):
+        boxes = traffic_light_detector.predict(image)
+        if len(boxes) > 0:
+            current_box = boxes[0]
+
+            if current_box.get_score() > self.SCORE_THRESHOLD:
+                # First time semaphore is detected
+                if self.prev_semaphore_box == None:
+                    self.prev_semaphore_box = current_box
+                    self.count_semaphore_detections = 1
+                else:
+                    # Check if the boxes refer to the same object
+                    xmin_diff = abs(self.camera_width * current_box.xmin - self.camera_width * self.prev_semaphore_box.xmin)
+                    xmax_diff = abs(self.camera_width * current_box.xmax - self.camera_width * self.prev_semaphore_box.xmax)
+                    ymin_diff = abs(self.camera_width * current_box.ymin - self.camera_width * self.prev_semaphore_box.ymin)
+                    ymax_diff = abs(self.camera_width * current_box.ymax - self.camera_width * self.prev_semaphore_box.ymax)
+
+                    if xmin_diff < self.th and xmax_diff < self.th and ymin_diff < self.th and ymax_diff < self.th:
+                        # the two boxes refer to the same object
+                        if current_box.get_label() == self.prev_semaphore_box.get_label():
+                            self.count_semaphore_detections += 1
+                        else:
+                            self.count_semaphore_detections = 1
+                    else:
+                        print("threshold violata")
+                        self.count_semaphore_detections = 1
+
+                    self.prev_semaphore_box = current_box
+
+                self.count_missdetection = 0
+            else:
+                self.count_missdetection += 1
+        else:
+            self.count_missdetection += 1
+            if self.count_missdetection == int(0.3*self.NUM_SEMAPHORE_CHECKS):
+                self.prev_semaphore_box = None
+                self.count_semaphore_detections = 0      
+                self.count_missdetection = 0
+
+        if self.count_semaphore_detections == self.NUM_SEMAPHORE_CHECKS:
+            # TODO: pass depth camera data from the main
+            depth_data = sensor_data['CameraDepth']
+            depth_data = depth_to_array(depth_data)
+
+            xmin = self.camera_width*current_box.xmin
+            ymin = self.camera_height*current_box.ymin
+            xmax = self.camera_width*current_box.xmax
+            ymax = self.camera_height*current_box.ymax
+
+            xmin = xmin - (xmax-xmin)
+            xmax = xmax + (xmax-xmin)
+            ymin = ymin - (ymax-ymin)
+            ymax = ymax + (ymax-ymin)
+
+            # From pixel to waypoint
+            depth = 1000 #Distance of the sky
+            for i in range(int(xmin), int(xmax+1)):
+                for j in range(int(ymin), int(ymax+1)):
+                    if j < 416 and i < 416:
+                        if depth > depth_data[j][i]:
+                            # Projection Pixel to Image Frame
+                            y = j
+                            x = i
+                            depth = depth_data[y][x] * 1000  # Consider depth in meters
+
+
+            # From pixel to waypoint
+            pixel = [x , y, 1]
+            pixel = np.reshape(pixel, (3,1))
+            
+
+            # Projection Pixel to Image Frame
+            depth = depth_data[y][x] * 1000  # Consider depth in meters  
+            stopsign_fences = []     # [x0, y0, x1, y1]
+            if depth != 1000.0:
+
+                image_frame_vect = np.dot(self.inv_intrinsic_matrix, pixel) * depth
+                
+                # Create extended vector
+                image_frame_vect_extended = np.zeros((4,1))
+                image_frame_vect_extended[:3] = image_frame_vect 
+                image_frame_vect_extended[-1] = 1
+                
+                # Projection Camera to Vehicle Frame
+                camera_frame = self.image_to_camera_frame(image_frame_vect_extended)
+                camera_frame = camera_frame[:3]
+                camera_frame = np.asarray(np.reshape(camera_frame, (1,3)))
+
+                camera_frame_extended = np.zeros((4,1))
+                camera_frame_extended[:3] = camera_frame.T 
+                camera_frame_extended[-1] = 1
+
+                camera_to_vehicle_frame = np.zeros((4,4))
+                camera_to_vehicle_frame[:3,:3] = to_rot([0, 0, 0])
+                camera_to_vehicle_frame[:,-1] = [self.cam_x_pos, self.cam_y_pos, self.cam_height, 1]
+
+                vehicle_frame = np.dot(camera_to_vehicle_frame, camera_frame_extended)
+                vehicle_frame = vehicle_frame[:3]
+                vehicle_frame = np.asarray(np.reshape(vehicle_frame, (1,3)))
+                
+                # TODO: call from the main
+                current_x, current_y, _, _, _, current_yaw = \
+                    get_current_pose(measurement_data)
+
+                stopsign_data = CUtils()
+                if (int(round(abs(cos(current_yaw))))):
+                    stopsign_data.create_var('x', vehicle_frame[0][0])
+                    stopsign_data.create_var('y', vehicle_frame[0][1])
+                else:
+                    stopsign_data.create_var('x', vehicle_frame[0][1])
+                    stopsign_data.create_var('y', vehicle_frame[0][0])
+                stopsign_data.create_var('z', vehicle_frame[0][2])
+
+                # obtain stop sign fence points for LP
+                x = stopsign_data.x
+                y = stopsign_data.y
+
+                spos = np.array([
+                        [current_x-5*int(round(abs(sin(current_yaw)))), current_x+5*int(round(abs(sin(current_yaw))))],
+                        [current_y-5*int(round(abs(cos(current_yaw)))), current_y+5*int(round(abs(cos(current_yaw))))]])
+                spos_shift = np.array([
+                        [x, x],
+                        [y, y]])
+
+                if np.sign(round(np.cos(current_yaw))) > 0: #mi sto muovendo lungo le x positive, verso destra
+                    spos = np.add(spos, spos_shift)
+                elif np.sign(round(np.cos(current_yaw))) > 0: #mi sto muovendo lungo le x negative, verso sinistra
+                    spos = np.subtract(spos, spos_shift)
+                else:
+                    if np.sign(round(np.sin(current_yaw))) > 0: #mi sto muovendo lungo le y positive, verso il basso
+                        spos = np.add(spos, spos_shift)
+                    else:
+                        spos = np.subtract(spos, spos_shift)
+
+                stopsign_fences.append([spos[0,0], spos[1,0], spos[0,1], spos[1,1]])
+                print("FENCES")
+                print(stopsign_fences)
+
+                self.prev_semaphore_box = None
